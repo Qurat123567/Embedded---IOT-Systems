d929d68c690f0ecf8dd2f679293cbf6f42ae3c06
Qura-tul-ain <qurat0149@gmail.com>
2025-10-25 12:15:36 +0500

    Added

---
+++ new12/src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+#include <Arduino.h>
+
+const int buttonPin = 25;
+const int ledPin = 0;
+volatile bool ledState = LOW;
+
+void IRAM_ATTR handleButton() {
+  ledState = !ledState;
+  digitalWrite(ledPin, ledState);
+}
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), handleButton, FALLING);
+}
+
+void loop() {
+  // main loop free for other tasks
+}
